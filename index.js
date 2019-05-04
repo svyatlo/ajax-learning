aa487aa1cd334d8d0fca86e181031ae6e88b4feb
Sviatlana Tamashevich <Sviatlana_Tamashevich@epam.com>
2019-05-04 17:40:20 +0300

    create function ajaxRequest

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-//$('#content').text('I\'m jQuery');
+
+function processData(response, findThis) {
+  var items = [];
+  var filmsArray = response.data;
+  console.log('response ', response);
+  console.log('filmsArray ', filmsArray);
+  console.log(findThis);
+  $.each( filmsArray, function() {
+    var title = this.title;
+    var genres = this.genres;
+    if (genres.includes(findThis)) {
+      items.push(this);
+    }
+  });
+  console.log(items);
+}
+
+function ajaxRequest(url) {
+  $.ajax({
+    url: url,
+    dataType: 'json',
+    data: { offset: 0, limit: 30 },
+    success: processData
+  });
+}
 
 $(function() {
-	$('#form').submit(function(e) {
-		e.preventDefault();
+  $('#form').submit(function(e) {
+    e.preventDefault();
 
-		var findThis = $(this).find('#findThis').val();
-		var url = 'http://react-cdp-api.herokuapp.com/movies/';
-
-		$.getJSON(url, {
-			offset: 0,
-			limit: 30
-		})
-			.done(function( response ) {
-				var filmsArray = response.data;
-				var items = [];
-				console.log('response ', response);
-				console.log('filmsArray ', filmsArray);
-				console.log(findThis);
-				$.each( filmsArray, function() {
-					var title = this.title;
-					var genres = this.genres;
-					if (genres.includes(findThis)) {
-						items.push(this);
-					}
-				});
-				console.log(items);
-			});
-	});
+    var findThis = $(this).find('#findThis').val();
+    var url = 'http://react-cdp-api.herokuapp.com/movies/';
+    console.log(findThis);
+    ajaxRequest(url);
+  });
 });
+
+
+// $.getJSON(url, {
+//   offset: 0,
+//   limit: 30
+// })
+//   .done(function( response ) {
+//     });
+//     console.log(items);
+//   });
