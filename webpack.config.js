0f3287a52588619f88af1bd27d58cc06f776f4d6
Sviatlana Tamashevich <Sviatlana_Tamashevich@epam.com>
2019-05-14 12:58:19 +0300

    feat: add package.json webpack .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js'
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Development'
+		})
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	}
+};
