e0a353bc47e4cdea1bbb0a0a55afd3f18f0d81d9
Sviatlana Tamashevich <sviatlana_tamashevich@epam.com>
2019-05-17 09:21:46 +0300

    feat: add modal

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function cutOverview(text) {
   if (text.length > 50) {
     text = text.substring(0, 50) + '...';
   }
-  return '<p>' + text + '<span class="read-more">>> read more</span></p>';
+  return text;
 }
 
 function createCard(item) {
@@ -17,34 +17,53 @@ function createCard(item) {
   const time = getTimeInHours(item.runtime);
   const overview = cutOverview(item.overview);
   return '<div class="content__item">'
-              + '<img src="' + item.poster_path + '" />'
-              + '<div class="item-description">'
-                + '<div class="item-title"><h2>' + item.title + '</h2><p>' + item.tagline + '</p></div>'
-                + '<div class="item-info"><h3>Genre: </h3><p>' + genres + '</p></div>'
-                + '<div class="item-info"><h3>Time: </h3><p>' + time + '</p></div>'
-                + '<div class="item-info"><h3>Rating: </h3><p>' + item.vote_average + '<span>(Votes: ' + item.vote_count + ')</span></p></div>'
-                + '<div class="item-info"><h3>Description: </h3>' + overview + '</div>'
-              + '</div>'
+            + '<img src="' + item.poster_path + '" />'
+            + '<div class="item-description">'
+              + '<div class="item-title"><h2>' + item.title + '</h2><p>' + item.tagline + '</p></div>'
+              + '<div class="item-info"><h3>Genre: </h3><p>' + genres + '</p></div>'
+              + '<div class="item-info"><h3>Time: </h3><p>' + time + '</p></div>'
+              + '<div class="item-info"><h3>Rating: </h3><p>' + item.vote_average + '<span>(Votes: ' + item.vote_count + ')</span></p></div>'
+              + '<div class="item-info"><h3>Description: </h3><p>' + overview + '<span class="read-more" id="' + item.id + '">>> read more</span></p></div>'
+            + '</div>'
           + '</div>';
 }
 
+function createModal(item) {
+  const genres = item.genres.join(', ');
+  const time = getTimeInHours(item.runtime);
+  return '<div class="wrapper__modal" id="' + item.id + '">'  
+          + '<div class="modal">'
+            + '<img src="' + item.poster_path + '" />'
+            + '<div class="item-description">'
+              + '<div class="item-info modal-title"><div class="item-title"><h2>' + item.title + '</h2><p>' + item.tagline + '</p></div><span class="close-modal">x</span></div>'
+              + '<div class="item-info"><h3>Genre: </h3><p>' + genres + '</p></div>'
+              + '<div class="item-info"><h3>Time: </h3><p>' + time + '</p></div>'
+              + '<div class="item-info"><h3>Rating: </h3><p>' + item.vote_average + '<span>(Votes: ' + item.vote_count + ')</span></p></div>'
+              + '<div class="item-info"><h3>Description: </h3><p>' + item.overview + '</p></div>'
+            + '</div>'
+          + '</div>'
+        + '</div>';
+}
+
 function renderCards(array) {
-  $.each(array, function() {
-    $('#content').append(this);
-  });
+  let string = array.join('');
+  
+  $('#content')
+    .append('<div class="cover"></div>')
+    .append(string);
 }
 
 function processData(response) {
   const responseArr = response.data;
   const data = this.data;
-  const findedItems = this.data.findedItems;
+  const cards = this.data.cards;
+  const modals = this.data.modals;
   const findThis = $('#findThis').val().toLowerCase();
 
   data.inProgress = false;
 
   console.log('response ', response);
   console.log('responseArr ', responseArr);
-  console.log('findThis ', findThis);
   
   for (let i = 0; i < responseArr.length; i += 1) {
     let title = responseArr[i].title.toLowerCase();
@@ -57,23 +76,40 @@ function processData(response) {
 
     if (title.includes(findThis) || description.includes(findThis) || genres.includes(findThis)) {
       const card = createCard(responseArr[i]);
-      findedItems.push(card);
+      const modal = createModal(responseArr[i]);
+      cards.push(card);
+      modals.push(modal);
     }
 
-    if (findedItems.length === 10) {
+    if (cards.length === data.limit) {
       data.offset += i + 1;
-      renderCards(findedItems);
+      renderCards(cards);
+      renderCards(modals);
       break;
     }
   }
   
-  if (findedItems.length < 10) {
+  if (cards.length < data.limit) {
     data.offset += data.limit;
     ajaxRequest(data);
   }
-
   console.log('data.offset ', data.offset);
-  console.log('findedItems ', findedItems);
+  
+  $('.read-more')
+  .click(function(e) {
+    let cardId = '#' + e.target.id + '.wrapper__modal';
+    $('body').addClass('lock');
+    $('.cover').slideDown(350);
+    $(cardId)
+    .slideDown(350);
+
+    $('.close-modal')
+      .click(function() {
+        $('body').removeClass('lock');
+        $('.cover').slideUp(350);
+        $(cardId).slideUp(350);
+      });
+  });
 }
 
 function ajaxRequest(data) {
@@ -93,8 +129,9 @@ $(function() {
   const data = {
     url: 'http://react-cdp-api.herokuapp.com/movies/',
     offset: 0,
-    limit: 10,
-    findedItems: [],
+    limit: 12,
+    cards: [],
+    modals: [],
     inProgress: false
   };
   
@@ -104,7 +141,8 @@ $(function() {
     .submit(function(e) {
       e.preventDefault();
       $('#content').empty();
-      data.findedItems = [];
+      data.cards = [];
+      data.modals = [];
       data.offset = 0;
       ajaxRequest(data);
     });
@@ -112,7 +150,8 @@ $(function() {
   $(window)
     .scroll(function() {
       if ($(window).scrollTop() + $(window).height() >= $(document).height() - 20 && !data.inProgress) {
-        data.findedItems = [];
+        data.cards = [];
+        data.modals = [];
         ajaxRequest(data);
       }
     });
