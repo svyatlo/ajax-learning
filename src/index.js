8e7758c92c5c2409653a63b80363f3dfcd6e2e92
Sviatlana Tamashevich <sviatlana_tamashevich@epam.com>
2019-07-05 18:02:06 +0300

    add request

--- src/index.js
+++ src/index.js
@@ -6,15 +6,41 @@ class RenderFilms extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      films: 0,
+      filmsCollection: {},
+      offset: 0,
+      limit: 12,
+      inProgress: false
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    const url = 'http://react-cdp-api.herokuapp.com/movies/';
+    const { offset, limit } = this.state;
+    e.preventDefault();
+    console.log('props', this.props);
+    this.setState({inProgress: true});
+
+    fetch(`${url}?offset=${offset}&limit=${limit}`)
+    .then(response => response.json())  
+    .then(result => {
+      this.setState({filmsCollection: result, offset: this.state.offset + this.state.limit, inProgress: false})
+      console.log('Request succeeded', this.state.filmsCollection);
+      console.log(this.state);
+    }).catch(function(error) {
+      console.log('Request failed', error);  
+    });
   }
 
   render() {
     return (
       <div>
-
-        <button onClick={makeRequest}>Search</button>
+        <form onSubmit={this.handleSubmit}>
+          <input id="findThis" type="text" name="findThis" />
+          <input type="submit" value="Search" />
+        </form>
+        <div className="content"></div>
       </div>
     )
   }
--- src/makeRequest.js
+++ src/makeRequest.js
@@ -1,24 +1,44 @@
-import createJson from './createJson';
+// import createJson from './createJson';
 
-const data = {
-  url: 'http://react-cdp-api.herokuapp.com/movies/',
-  offset: 0,
-  limit: 12,
-  cards: [],
-  modals: [],
-  inProgress: false
-};
+// function makeRequest(url, offset, limit, inProgress) {
+//   inProgress = true;
+//   console.log(inProgress);
+//   fetch(`${url}?offset=${offset}&limit=${limit}`)
+//   .then(createJson)  
+//   .then(function(data) {  
+//     console.log('Request succeeded with JSON response', data);
+//   }).catch(function(error) {
+//     console.log('Request failed', error);  
+//   });
+// }
 
-function makeRequest() {
-  data.inProgress = true;
-  console.log(data.inProgress);
-  fetch(`${data.url}?offset=${data.offset}&limit=${data.limit}`)
-  .then(createJson)  
-  .then(function(data) {  
-    console.log('Request succeeded with JSON response', data);
-  }).catch(function(error) {
-    console.log('Request failed', error);  
-  });
-}
+// export default makeRequest;
+// import React from 'react';
 
-export default makeRequest;
+// export default class Forma extends React.Component {
+//   constructor(props) {
+//       super(props);
+
+//       this.state = { data: {}, isFetching: true, error: null };
+//   }
+
+//   componentDidMount() {
+//       fetch('http://react-cdp-api.herokuapp.com/movies/')
+//           .then(response => response.json())
+//           .then(result => this.setState({data: result, isFetching: false }))
+//           .catch(e => {
+//             console.log(e);
+//             this.setState({data: result, isFetching: false, error: e });
+//           });
+//   }
+
+//   render() {
+//       const { data, isFetching, error } = this.state;
+      
+//       if (isFetching) return <div>...Loading</div>;
+
+//       if (error) return <div>{`Error: ${e.message}`}</div>;
+
+//       return <h1>{data.goals[0].gs_id}</h1>;
+//   }
+// }
